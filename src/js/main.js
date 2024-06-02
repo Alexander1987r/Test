fe1336dfed45c24d4a3b093ffd961ae470157fe5
Alexander1987r <alexnext7771987r@rambler.ru>
2024-06-02 15:09:36 +0300

    add slider products

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,11 @@
 import { getBurger } from "./burger";
 import { getMap } from "./map";
 import { getAccordion} from "./accordion"
+import { getSwiper } from "./swiper";
 
-import Swiper from 'swiper/bundle';
-
-// import styles bundle
-import 'swiper/css/bundle';
-
-
-const swiper= new Swiper('.career__list',{
-
-  navigation:{
-      prevEl:'.career__button-prev',
-      nextEl:'.career__button-next'
-  },
-  pagination:{
-      el:'.products__pagination',
-      clickable:true,
-  },
-
-});
 
 
 getAccordion();
 getBurger();
 getMap();
-
+getSwiper();
---
+++ src/js/swiper.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+
+
+export const getSwiper=()=>{
+  const swiperProducts= new Swiper('.products__slide',{
+    direction:'horizontal',
+    loop:true,
+    speed:1000,
+    effect:'slider',
+    slidesPerView:2,
+    centeredSlides:true,
+    spaceBetween:20,
+    navigation:{
+      prevEl:'.products__slide-btn-prev',
+      nextEl:'.products__slide-btn-next'
+    },
+    pagination:{
+      el:'.products__pagination',
+      clickable:true,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      992: {
+        spaceBetween: 75
+      },
+    }
+
+  });
+  const swiper= new Swiper('.career__list',{
+  navigation:{
+      prevEl:'.career__button-prev',
+      nextEl:'.career__button-next'
+  },
+  });
+}
+
